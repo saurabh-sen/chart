3e0cc1a56b2153e8a410bd410d09dc628277e2ab
Saurabh Sen <sensaurabh1212@gmail.com>
2023-04-01 15:25:24 +0530

    refactored

--- admin/src/pages/HomePage/index.js
+++ admin/src/pages/HomePage/index.js
@@ -4,10 +4,10 @@
  *
  */
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 // import PropTypes from 'prop-types';
-import pluginId from "../../pluginId";
-import "./styles.css";
+import pluginId from '../../pluginId';
+import './styles.css';
 import LineChart from "../../components/LineChart";
 import { CategoryScale } from "chart.js";
 import Chart from "chart.js/auto";
@@ -15,6 +15,7 @@ import Chart from "chart.js/auto";
 Chart.register(CategoryScale);
 
 const HomePage = () => {
+
   const [chartData, setChartData] = useState({
     labels: [],
 
@@ -27,36 +28,21 @@ const HomePage = () => {
           "#ecf0f1",
           "#f0331a",
           "#f3ba2f",
-          "#2a71d0",
+          "#2a71d0"
         ],
         borderColor: "red",
-        borderWidth: 2,
-      },
-    ],
+        borderWidth: 2
+      }
+    ]
   });
 
+
+  
   return (
-    <div className="main">
+    <div className='main'>
       <h1>{pluginId}&apos;s HomePage</h1>
       <p>Happy coding lorem2 </p>
-      {/* <LineChart chartData={chartData} /> */}
-      <div className="chart-container chartArea">
-        <h2 style={{ textAlign: "center" }}>Sales Analytics</h2>
-        <Line
-          data={chartData}
-          options={{
-            plugins: {
-              title: {
-                display: true,
-                text: "Analytics",
-              },
-              legend: {
-                display: true,
-              },
-            },
-          }}
-        />
-      </div>
+      <LineChart chartData={chartData} />
     </div>
   );
 };
